58628b7d970c1d99f5c80643d3de4cb6279c7470
majeo69 <alexgutscher26@gmail.com>
2020-08-26 01:27:24 -0500

    initial commit

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+// View -> UIVIEW
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
